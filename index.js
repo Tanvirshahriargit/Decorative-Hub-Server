08adfeb733f0d703845c682d8d203a9556d22dd6
Tanvirshahriargit <tanvirshahriar20188@gmail.com>
2021-11-13 23:19:55 +0600

    Update Status

--- index.js
+++ index.js
@@ -90,6 +90,19 @@ async function run() {
       res.json(result)
     })
 
+    // status update
+  app.put("/orders/:id", async (req, res) => {
+    const filter = { _id: ObjectId(req.params.id) };
+    console.log(req.params.id);
+    const result = await ordersCollection.updateOne(filter, {
+      $set: {
+        status: req.body.status,
+      },
+    });
+    res.send(result);
+    console.log(result);
+  });
+
     // Admin Check 
     app.get('/users/:email', async (req, res) => {
       const email = req.params.email;
